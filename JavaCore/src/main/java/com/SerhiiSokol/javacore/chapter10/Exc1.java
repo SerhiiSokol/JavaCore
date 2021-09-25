cfde3624d7f7301fd840f7d4cc195bf15ffad1d1
Serhii Sokolov <Sokolovsv91@gmail.com>
2021-09-25 22:18:45 +0300

    Test

---
+++ JavaCore/src/main/java/com/SerhiiSokol/javacore/chapter10/Exc1.java
@@ -0,0 +1,12 @@
+package main.java.com.SerhiiSokol.javacore.chapter10;
+
+class Exc1 {
+    static void subroutine() {
+        int d = 0;
+        int а = 10 / d;
+    }
+
+    public static void main(String[] args) {
+        Exc1.subroutine();
+    }
+}
